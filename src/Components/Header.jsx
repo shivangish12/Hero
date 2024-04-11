6462837e239a3e80c42fba50965ed390da6aee5c
Shivangi Sharma <shanussh@gmail.com>
2024-04-11 21:56:39 +0530

    without module

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import styles from "./Header.module.css";
 const Header = () => {
   return (
-    <div className={styles.header}>
+    <div>
       <p>
         The health and well-being of our patients and their health care team
         will always be our priority, so we follow the best practices for
