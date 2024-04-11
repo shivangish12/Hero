f126563c7a901028ff9dd04feda0631b8202edfb
Shivangi Sharma <shanussh@gmail.com>
2024-04-11 22:17:03 +0530

    app module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
-import "./App.css";
+import styles from "./App.module.css";
 import Header from "./Components/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Header />
     </div>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,10 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "#2aa7ff",
-        width: "100%",
-        height: "40px",
-        gap: "0px",
-        opacity: "0px",
-      }}
-    >
-      <p
-        style={{
-          fontFamily: "Poppins",
-          fontSize: "14px",
-          fontWeight: "400",
-          lineHeight: "40px",
-          color: "white",
-          textAlign: "center",
-        }}
-      >
+    <div className={styles.header}>
+      <p>
         The health and well-being of our patients and their health care team
         will always be our priority, so we follow the best practices for
         cleanliness.
