5dc71d3a69f2ad1383a7e019ec3f0452049eaec3
Shivangi Sharma <shanussh@gmail.com>
2024-04-11 22:07:34 +0530

    inline css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import "./Header.css";
+
 const Header = () => {
   return (
-    <div className="header">
-      <p>
+    <div
+      style={{
+        backgroundColor: "#2aa7ff",
+        width: "100%",
+        height: "40px",
+        gap: "0px",
+        opacity: "0px",
+      }}
+    >
+      <p
+        style={{
+          fontFamily: "Poppins",
+          fontSize: "14px",
+          fontWeight: "400",
+          lineHeight: "40px",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
         The health and well-being of our patients and their health care team
         will always be our priority, so we follow the best practices for
         cleanliness.
